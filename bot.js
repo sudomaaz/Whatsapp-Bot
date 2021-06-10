3cc52fe625b24929f871bb4a076eb59319b7f381
sudomaaz <maazm0649@gmail.com>
2021-06-10 21:21:16 +0530

    aded fix

--- bot.js
+++ bot.js
@@ -1255,6 +1255,7 @@ async function connectAndRunBot() {
     //called when some group join/remove action occurs
     conn.on("group-participants-update", async (group) => {
       if (group.action === "add") {
+        return;
         const groupMetaData = await conn.groupMetadata(group.jid);
         const gname = groupMetaData.subject.trim();
         const gusers = groupMetaData.participants.length;
